48ccddd80f710b2376265a10c7e8133f576f4e93
Mariano Pareja <mariano@Marianos-MacBook-Pro.local>
2024-01-08 16:30:21 -0400

    update front end

--- base/models.py
+++ base/models.py
@@ -1,43 +1,46 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
 
 
 class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
-    email = models.EmailField( unique=True, null=True)
+    email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
 
     avatar = models.ImageField(null=True, default="avatar.svg")
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def create_superuser(self, username, email, password):
+        pass
 
 
 # Create your models here.
 
-class Topic(models.Model): 
+
+class Topic(models.Model):
     name = models.CharField(max_length=200)
 
-    def __str__(self): 
+    def __str__(self):
         return self.name
+
 
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    participants = models.ManyToManyField(
-        User, related_name='participants', blank=True)
+    participants = models.ManyToManyField(User, related_name="participants", blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-    class Meta: 
-        ordering = ['-updated', '-created']
+    class Meta:
+        ordering = ["-updated", "-created"]
 
     def __str__(self):
         return self.name
-    
+
 
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -46,9 +49,8 @@ class Message(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-    class Meta: 
-        ordering = ['-updated', '-created']
+    class Meta:
+        ordering = ["-updated", "-created"]
 
-    def __str__(self): 
+    def __str__(self):
         return self.body[0:50]
-
