33b97bb412d734ed45d86ee00d47434dc0c13c57
Mariano Pareja <mariano@Marianos-MacBook-Pro.local>
2024-08-06 20:35:18 -0400

    update deploy script

--- scripts/deploy_ecr.sh
+++ scripts/deploy_ecr.sh
@@ -5,19 +5,30 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_FOLDER_DIR="$(dirname "$SCRIPT_DIR")"
 
 # Deploy ECR repository
-cd ${PROJECT_FOLDER_DIR}/infrastructure/terraform
-terraform plan -target=module.ecr -out=ecr-plan.out
-terraform apply "ecr-plan.out"
+cd "${PROJECT_FOLDER_DIR}/infrastructure/terraform" || log_and_exit "Failed to change directory to Terraform folder"
 
-# Authenticate to AWS
-aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+terraform plan -target=module.ecr -out=ecr-plan.out || log_and_exit "Terraform plan failed"
+terraform apply "ecr-plan.out" || log_and_exit "Terraform apply failed"
 
-# Build and push docker containers
-cd $PROJECT_FOLDER_DIR
-docker-compose build
+# Authenticate to AWS ECR
+echo "Authenticating to ECR..."
+if ! aws ecr get-login-password --region "${AWS_REGION}" | docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"; then
+  exit_code=$?  # Capture the exit code of the previous command
+  log_and_exit "Docker login to ECR failed (exit code $exit_code)"
+fi
 
-docker tag roottoroot-nginx ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ecr-repo:nginx-latest
-docker tag roottoroot-django-gunicorn ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ecr-repo:guinicorn-latest
+# Build and push Docker containers
+cd "$PROJECT_FOLDER_DIR" || log_and_exit "Failed to change directory to project root"
 
-docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ecr-repo:nginx-latest
-docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ecr-repo:guinicorn-latest
+echo "Building Docker images..."
+docker-compose build || log_and_exit "Docker build failed"
+
+echo "Tagging Docker images..."
+# docker tag roottoroot-nginx "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ecr-repo:nginx-latest" || log_and_exit "Failed to tag nginx image"
+docker tag roottoroot-gunicorn "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ecr-repo:guinicorn-latest" #|| log_and_exit "Failed to tag gunicorn image"
+
+echo "Pushing Docker images to ECR..."
+# docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ecr-repo:nginx-latest" || log_and_exit "Failed to push nginx image"
+docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/ecr-repo:guinicorn-latest" #|| log_and_exit "Failed to push gunicorn image"
+
+echo "Deployment completed successfully!"
